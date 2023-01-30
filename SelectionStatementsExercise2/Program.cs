61956a4f466b879993cceb4fa0bdd33b47582a4e
TonyDolenti <antoniobdolenti@gmail.com>
2023-01-30 07:21:52 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "history":
+                    Console.WriteLine("That's the best subject, no debate.");
+                    break;
+                case "math":
+                    Console.WriteLine("Math is terrible.");
+                    break;
+                case "phys ed":
+                case "gym":
+                    Console.WriteLine("Only other acceptable answer.");
+                    break;
+                case "english":
+                    Console.WriteLine("This one is the worst and you're definitely a nerd.");
+                    break;
+                
+                default:
+                    Console.WriteLine("I've never heard of this one.");
+                    break;
+
+
+            }
         }
     }
 }
